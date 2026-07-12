cf76883a892a33f2c453bdb12c75d17d85319c7d
Quastrado <Ataxyswriter@yandex.ru>
2026-07-12 18:40:22 +0300

    initial and schemas

---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import net.datafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationTests {
+
+    @Test
+    public void successfulRegistrationTest(){
+        Faker faker = new Faker();
+    }
+}
